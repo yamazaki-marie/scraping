ae13ead7c6a83083ac73d4f96de2d13145272024
GuestUser <guest@git test.co.jp>
2025-09-27 11:59:23 +0900

    divタグの内容を表示

--- scraping.py
+++ scraping.py
@@ -3,5 +3,5 @@ from bs4 import BeautifulSoup
 response=requests.get("https://www.google.com")
 #print(response.text)
 soup=BeautifulSoup(response.text, 'html.parser')
-title=soup.find('title').text
+title=soup.find('div').get_text()
 print(f"ページのタイトル{title}")
