53d5c30305df82743a973ec0f9a59e6a1ad30c0a
GuestUser <guest@git test.co.jp>
2025-09-27 11:54:38 +0900

    beautifulsoupでhtmlを解析

--- scraping.py
+++ scraping.py
@@ -1,3 +1,7 @@
 import requests
+from bs4 import BeautifulSoup
 response=requests.get("https://www.google.com")
-print(response.text)
+#print(response.text)
+soup=BeautifulSoup(response.text, 'html.parser')
+title=soup.find('title').text
+print(f"ページのタイトル{title}")
